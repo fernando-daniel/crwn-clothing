e4ae1eea6822aa14675ca2d57d257ac3e1481da6
fernandodcastanon <fernandodaniel777castanon@gmail.com>
2019-10-27 20:02:20 -0700

    Updates

---
+++ precache-manifest.a2b676692c4adbb2b8b70694cf2eb694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814754141b328432ce75de7731c584a3",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "d6f67810ca2ae256cf71",
+    "url": "/crwn-clothing/static/css/main.6d97f334.chunk.css"
+  },
+  {
+    "revision": "320e22a2be00d886c761",
+    "url": "/crwn-clothing/static/js/2.1dac7939.chunk.js"
+  },
+  {
+    "revision": "d6f67810ca2ae256cf71",
+    "url": "/crwn-clothing/static/js/main.b832a7cf.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
